8000654d948085ad70c9b6f0674d65c0c54faf2c
Naveen Hettiwaththa <naveenhettiwaththa@gmail.com>
2022-10-03 00:51:31 +0530

    added nav links

--- components/layouts/AppDrawer.tsx
+++ components/layouts/AppDrawer.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import {
+    List,
+    ListItem,
+    ListIcon,
+    OrderedList,
+    UnorderedList,
+    Divider,
     Button,
     Drawer,
     DrawerBody,
@@ -11,15 +17,16 @@ import {
 } from '@chakra-ui/react'
 
 import { HamburgerIcon } from '@chakra-ui/icons'
+import { BsHouseDoor , BsCalendarWeek , BsArchive , BsGear} from 'react-icons/bs';
 
 interface prop {
-    isOpen : boolean;
-    handleClose : any
+    isOpen: boolean;
+    handleClose: any
 }
 
-function AppDrawer(props:prop) {
+function AppDrawer(props: prop) {
 
-    const {isOpen , handleClose} = props;
+    const { isOpen, handleClose } = props;
 
     return (
         <>
@@ -32,20 +39,45 @@ function AppDrawer(props:prop) {
                 placement='left'
                 onClose={() => handleClose(false)}
             >
-                
+
                 <DrawerOverlay />
 
-                <DrawerContent>
+                <DrawerContent className='p-5'>
                     {/* <DrawerCloseButton /> */}
-                    <DrawerHeader>Create your account</DrawerHeader>
 
-                    
-                    <DrawerFooter>
+                    <List spacing={1}>
+
+                        <ListItem className=' cursor-pointer flex flex-row bg-slate-300 p-4 rounded-r-[40px]'>
+                            <BsHouseDoor size={20} className='my-auto mr-5' />
+                            <h1 className=' text-lg font-medium'>Classes</h1>
+                        </ListItem>
+
+                        <ListItem className=' cursor-pointer flex flex-row hover:bg-slate-300 p-4 rounded-r-[40px]'>
+                            <BsCalendarWeek size={20} className='my-auto mr-5' />
+                            <h1 className=' text-lg font-medium'>Calendar</h1>
+                        </ListItem>
+
+                        <Divider className='mt-2' orientation='horizontal' />
+
+                        <ListItem className=' cursor-pointer flex flex-row hover:bg-slate-300 p-4 rounded-r-[40px]'>
+                            <BsArchive size={20} className='my-auto mr-5' />
+                            <h1 className=' text-lg font-medium'>Archived classes</h1>
+                        </ListItem>
+
+                        <ListItem className=' cursor-pointer flex flex-row hover:bg-slate-300 p-4 rounded-r-[40px]'>
+                            <BsGear size={20} className='my-auto mr-5' />
+                            <h1 className=' text-lg font-medium'>Settings</h1>
+                        </ListItem>
+
+                    </List>
+
+
+                    {/* <DrawerFooter>
                         <Button variant='outline' mr={3} onClick={() => handleClose(false)}>
                             Cancel
                         </Button>
                         <Button colorScheme='blue'>Save</Button>
-                    </DrawerFooter>
+                    </DrawerFooter> */}
 
                 </DrawerContent>
 
