5f6312f8b99cb0f50e618720b287bf990b250bcb
Naveen Hettiwaththa <naveenhettiwaththa@gmail.com>
2022-10-03 06:53:01 +0530

    added latest changes

--- components/Home/Body.tsx
+++ components/Home/Body.tsx
@@ -1,19 +1,48 @@
 import React from 'react'
 import Image from 'next/image'
+import { Button } from '@chakra-ui/react'
+import { useAuth } from '../../context/AuthUserContext';
 
 function Body() {
+
+  const { authUser, loading } = useAuth();
+
   return (
     <div className='flex flex-1 justify-center items-center h-[90vh]'>
-      <div>
-        <Image
-          className='my-auto'
-          src="/img/landing.gif"
-          alt="Picture of the author"
-          width={250}
-          height={250}
-        />
-        <h1 className='text-center font-medium text-xl'>Join now to get started today!</h1>
-      </div>
+
+      {!authUser && (
+        <div>
+          <Image
+            className='my-auto'
+            src="/img/landing.gif"
+            alt="Picture of the author"
+            width={280}
+            height={280}
+          />
+          <h1 className='text-center font-medium text-xl'>Join now to get started today!</h1>
+        </div>
+      )}
+
+      {authUser && (
+        <div>
+          <Image
+            className='my-auto'
+            src="/img/authLanding.gif"
+            alt="Picture of the author"
+            width={280}
+            height={280}
+          />
+          <h1 className='text-center font-medium text-md'>Add a class to get started</h1>
+          <div className='mt-5 flex justify-between flex-row'>
+            <Button colorScheme='twitter' variant='ghost'>Create class</Button>
+            <Button colorScheme='twitter' variant='solid'>Join class</Button>
+          </div>
+        </div>
+      )}
+
+
+
+
     </div>
   )
 }
