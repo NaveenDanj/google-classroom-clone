8b693a4e5595621c288ca64ccf9aef9d4e3cf0ff
Naveen Hettiwaththa <naveenhettiwaththa@gmail.com>
2022-10-05 23:42:07 +0530

    added people card component

---
+++ components/Classroom/People/PeopleCard.tsx
@@ -0,0 +1,31 @@
+import { Avatar, Box, IconButton } from '@chakra-ui/react'
+import React from 'react'
+import { BsFillFileEarmarkTextFill, BsThreeDotsVertical } from 'react-icons/bs'
+
+const PeopleCard = () => {
+    return (
+        <Box className='p-2 mt-2  pb-3' borderRadius='lg' overflow='hidden'>
+
+            <div className='flex flex-row w-full justify-between cursor-pointer'>
+
+                <div className='flex mt-2 flex-row'>
+
+                    <Avatar size='sm' name='Kent Dodds' src='https://bit.ly/kent-c-dodds' />
+
+                    <h2 className='ml-2 w-full cursor-pointer shrink-0 text-sm my-auto  font-medium'>
+                        Varnarubiny Surendranathan
+                    </h2>
+
+                </div>
+
+                <div className='flex flex-row'>
+                    <IconButton className='my-auto' size={'sm'} aria-label='Search database' icon={<BsThreeDotsVertical />} />
+                </div>
+
+            </div>
+
+        </Box>
+    )
+}
+
+export default PeopleCard
--- components/Classroom/People/StudentPeopleView.tsx
+++ components/Classroom/People/StudentPeopleView.tsx
@@ -1,17 +1,28 @@
 import { IconButton } from '@chakra-ui/react'
 import React from 'react'
 import { BsFillPersonPlusFill } from 'react-icons/bs'
+import PeopleCard from './PeopleCard'
 
 const StudentPeopleView = () => {
     return (
-        <div className='flex border-b-2 border-orange-600 flex-row justify-between mt-5 p-5 w-full'>
-            <div>
-                <h1 className=' text-2xl text-orange-600 '>Classmates</h1>
+        
+        <div className='w-full mt-5 '>
+            <div className='p-5 flex border-b-2 border-orange-600 flex-row justify-between '>
+                <div>
+                    <h1 className=' text-2xl text-orange-600 '>Classmates</h1>
+                </div>
+
+                <IconButton colorScheme={'orange'} variant="ghost" className='my-auto' size={'md'} aria-label='Search database' icon={<BsFillPersonPlusFill style={{ fontSize: 25 }} />} />
+
             </div>
 
-            <IconButton colorScheme={'orange'} variant="ghost" className='my-auto' size={'md'} aria-label='Search database' icon={<BsFillPersonPlusFill style={{ fontSize: 25 }} />} />
+            <div>
+                <PeopleCard />
+            </div>
+
 
         </div>
+
     )
 }
 
